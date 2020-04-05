70875e2bdda22088a068579bf6cad52a7bfe3017
Tulsi Das <tulsi.das@joonko.io>
2020-04-05 12:36:23 +0200

    test 3

---
+++ precache-manifest.bc698cc65d7ad45eea5e910aa0aed9d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a1307ec372dfe9a0e25d6025ed85d9",
+    "url": "/bilalshop/index.html"
+  },
+  {
+    "revision": "da7b967c5c73b3d0b56e",
+    "url": "/bilalshop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8523ab1293977e68e00b",
+    "url": "/bilalshop/static/js/2.5ade31d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bilalshop/static/js/2.5ade31d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7b967c5c73b3d0b56e",
+    "url": "/bilalshop/static/js/main.d1132264.chunk.js"
+  },
+  {
+    "revision": "ad0befcbaa459d6830cc",
+    "url": "/bilalshop/static/js/runtime-main.3e5e8ecc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bilalshop/static/media/logo.5d5d9eef.svg"
+  }
+]);
