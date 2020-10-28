cd57e71dc2051c722a3f546a2111bc4c6a438c9c
Tulsi Das <khatritulsidas@gmail.com>
2020-10-29 00:03:57 +0100

    updated api base url

---
+++ precache-manifest.05316f4b055ed0bbe44098ade3ee4758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a495739262e8a510dab05982a4d774dc",
+    "url": "/bilalshop/index.html"
+  },
+  {
+    "revision": "35613802248d38584af8",
+    "url": "/bilalshop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cb8ca013a748e137e598",
+    "url": "/bilalshop/static/js/2.33e681f2.chunk.js"
+  },
+  {
+    "revision": "78dac9c195d86504d7d5f59e90a9c301",
+    "url": "/bilalshop/static/js/2.33e681f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35613802248d38584af8",
+    "url": "/bilalshop/static/js/main.3b7eb24c.chunk.js"
+  },
+  {
+    "revision": "ad0befcbaa459d6830cc",
+    "url": "/bilalshop/static/js/runtime-main.3e5e8ecc.js"
+  }
+]);
