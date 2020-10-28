c03bbbc76b1962b71d01741e42bd65c547fddfab
Tulsi Das <khatritulsidas@gmail.com>
2020-10-29 00:49:55 +0100

    removed /bilalshop from url

---
+++ precache-manifest.20c4f5f4752a82571b55225d326ebaed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e3e8875d2d5ce45b18447c18f2acb3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2996e5231968a960d82a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2577bde7baa6c8c5a2c",
+    "url": "/static/js/2.b2e568f5.chunk.js"
+  },
+  {
+    "revision": "78dac9c195d86504d7d5f59e90a9c301",
+    "url": "/static/js/2.b2e568f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2996e5231968a960d82a",
+    "url": "/static/js/main.0b4efe2d.chunk.js"
+  },
+  {
+    "revision": "121b874a96e7d44cfbf1",
+    "url": "/static/js/runtime-main.5ab24f1e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bilalshop/precache-manifest.1327bd935e14f4a692b57646961333fe.js"
+  "/precache-manifest.20c4f5f4752a82571b55225d326ebaed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bilalshop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
